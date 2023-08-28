b8baafe77c0a4b474481602b17d8cf1594eee294
Paulo Victor Capuano <pvcapuano@gmail.com>
2023-08-28 19:20:52 -0300

    refactor: refactor all project

---
+++ .next/static/webpack/app/page.1fdd3940e63af90e.hot-update.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/page",{
+
+/***/ "(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5C55219%5CDesktop%5Cquizz-front%5Cnode_modules%5C%40mui%5Cmaterial%5Cnode%5Cindex.js&modules=C%3A%5CUsers%5C55219%5CDesktop%5Cquizz-front%5Csrc%5Ccomponents%5CLoginForm%5Cindex.tsx&server=false!":
+/*!*****************************************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5C55219%5CDesktop%5Cquizz-front%5Cnode_modules%5C%40mui%5Cmaterial%5Cnode%5Cindex.js&modules=C%3A%5CUsers%5C55219%5CDesktop%5Cquizz-front%5Csrc%5Ccomponents%5CLoginForm%5Cindex.tsx&server=false! ***!
+  \*****************************************************************************************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.t.bind(__webpack_require__, /*! ./node_modules/@mui/material/node/index.js */ \"(app-client)/./node_modules/@mui/material/node/index.js\", 23));\nPromise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./src/components/LoginForm/index.tsx */ \"(app-client)/./src/components/LoginForm/index.tsx\"))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1jbGllbnQpLy4vbm9kZV9tb2R1bGVzL25leHQvZGlzdC9idWlsZC93ZWJwYWNrL2xvYWRlcnMvbmV4dC1mbGlnaHQtY2xpZW50LWVudHJ5LWxvYWRlci5qcz9tb2R1bGVzPUMlM0ElNUNVc2VycyU1QzU1MjE5JTVDRGVza3RvcCU1Q3F1aXp6LWZyb250JTVDbm9kZV9tb2R1bGVzJTVDJTQwbXVpJTVDbWF0ZXJpYWwlNUNub2RlJTVDaW5kZXguanMmbW9kdWxlcz1DJTNBJTVDVXNlcnMlNUM1NTIxOSU1Q0Rlc2t0b3AlNUNxdWl6ei1mcm9udCU1Q3NyYyU1Q2NvbXBvbmVudHMlNUNMb2dpbkZvcm0lNUNpbmRleC50c3gmc2VydmVyPWZhbHNlISIsIm1hcHBpbmdzIjoiQUFBQSw2TUFBeUg7QUFDekgiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLz8yYWNjIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCgvKiB3ZWJwYWNrTW9kZTogXCJlYWdlclwiICovIFwiQzpcXFxcVXNlcnNcXFxcNTUyMTlcXFxcRGVza3RvcFxcXFxxdWl6ei1mcm9udFxcXFxub2RlX21vZHVsZXNcXFxcQG11aVxcXFxtYXRlcmlhbFxcXFxub2RlXFxcXGluZGV4LmpzXCIpO1xuaW1wb3J0KC8qIHdlYnBhY2tNb2RlOiBcImVhZ2VyXCIgKi8gXCJDOlxcXFxVc2Vyc1xcXFw1NTIxOVxcXFxEZXNrdG9wXFxcXHF1aXp6LWZyb250XFxcXHNyY1xcXFxjb21wb25lbnRzXFxcXExvZ2luRm9ybVxcXFxpbmRleC50c3hcIikiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5C55219%5CDesktop%5Cquizz-front%5Cnode_modules%5C%40mui%5Cmaterial%5Cnode%5Cindex.js&modules=C%3A%5CUsers%5C55219%5CDesktop%5Cquizz-front%5Csrc%5Ccomponents%5CLoginForm%5Cindex.tsx&server=false!\n"));
+
+/***/ })
+
+});
--- src/app/quizz/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Quizz = () => {
-  return <div>tela do quiz</div>;
-};
-
-export default Quizz;
